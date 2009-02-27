a8bc8dd555a8252af87bc88a2fa58d55fde20651
john warner <jcwarner@usgs.gov>
2009-02-27 03:33:30 +0000

    joe tc weights

--- Tools/mfiles/scrip_masking.m
+++ Tools/mfiles/scrip_masking.m
@@ -1,6 +1,7 @@
 % script scrip_masking.m
 %
-% adjusts the masking in the wrf scrip grid.
+% Adjusts the masking in the wrf scrip grid.
+% Determines what cells are not in the roms grid.
 %
 % jcw Sept 7, 2008
 %     Feb 20, 2009
@@ -9,44 +10,35 @@
 %%%%%%%%%% BEGIN user input   %%%%%%%%%%%%
 %
 %1) Enter name of roms netcdf grid file.
-grid_file = 'joe_tc_coarse_grd.nc';
+grid_filer = 'joe_tc_coarse_grd.nc';
 
-%2) Enter name of netcdf output file to use by scrip.
-out_file = 'joe_tc_coarse_roms_scrip.nc';
+%2) Enter name of file with WRF grid info.
+grid_filew = 'wrfinput_d01';
 
 %%%%%%%%%% END of user input   %%%%%%%%%%%%
 
-
-
-
-%4) Set masking in roms2wrf file
-% determine masking for wrf grid
-% what cells are not in the roms grid?
-ncclear
-
 %get roms grid
-ncload Z:\jwarner\Isabel\roms\forcings\USeast_grd3.nc
+ncload(grid_filer)
+if ((spherical=='F')||(spherical=='f'))
+  lon_rho=x_rho;
+  lat_rho=y_rho;
+end
 cornersx=[lon_rho(1,1) lon_rho(1,end) lon_rho(end,end) lon_rho(end,1)];
 cornersy=[lat_rho(1,1) lat_rho(1,end) lat_rho(end,end) lat_rho(end,1)];
 
 %get wrf grid
-ncload z:\jwarner\Isabel\roms\run106\xlongu.nc
-ncload z:\jwarner\Isabel\roms\run106\xlatv.nc
-LP=size(XLONG_U,2);
-MP=size(XLAT_V,1);
-lon_rho_wrf=repmat(XLONG_U(1,:),MP,1);
-lat_rho_wrf=repmat(XLAT_V(:,1),1,LP);
-
+nc=netcdf(grid_filew);
+lon_rho_wrf=nc{'XLONG'}(:);
+lat_rho_wrf=nc{'XLAT'}(:);
+[MP,LP]=size(lon_rho_wrf);
+ncclose('nc')
 
 %find wrf points inside roms grid
 wrfinroms=inpolygon(lon_rho_wrf,lat_rho_wrf,cornersx,cornersy);
-%save wrfmask.mat
 
-%cd Z:\jwarner\Isabel\roms\forcings
 %now put this into the mask array
-%load D:\data\Carolinas\hurricanes\Isabel\roms\run101\wrfmask.mat
-ncload Z:\jwarner\Isabel\roms\forcings\ROMS_USeast_grd3_to_WRF_grd3_weights.nc
-nc=netcdf('Z:\jwarner\Isabel\roms\forcings\ROMS_USeast_grd3_to_WRF_grd3_weights.nc','w')
+ncload ocn2atm_weights.nc
+nc=netcdf('ocn2atm_weights.nc','w');
 count=0;
 ztemp=zeros(LP*MP,1);
 for jj=1:MP
