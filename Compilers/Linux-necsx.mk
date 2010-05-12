8cda630f2e2a4ae46549120affc6a58685a6b70a
john warner <jcwarner@usgs.gov>
2010-05-12 02:33:23 +0000

    compilers

---
+++ Compilers/Linux-necsx.mk
@@ -0,0 +1,163 @@
+# svn $Id: Linux-necsx.mk 429 2009-12-20 17:30:26Z arango $
+#::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+# Copyright (c) 2002-2010 The ROMS/TOMS Group                           :::
+#   Licensed under a MIT/X style license                                :::
+#   See License_ROMS.txt                                                :::
+#::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+#
+# Include file for NEC SXF90 cross compiler on Linux
+# -------------------------------------------------------------------------
+#
+# ARPACK_LIBDIR  ARPACK libary directory
+# FC             Name of the fortran compiler to use
+# FFLAGS         Flags to the fortran compiler
+# CPP            Name of the C-preprocessor
+# CPPFLAGS       Flags to the C-preprocessor
+# CLEAN          Name of cleaning executable after C-preprocessing
+# NETCDF_INCDIR  NetCDF include directory
+# NETCDF_LIBDIR  NetCDF libary directory
+# LD             Program to load the objects into an executable
+# LDFLAGS        Flags to the loader
+# RANLIB         Name of ranlib command
+# MDEPFLAGS      Flags for sfmakedepend  (-s if you keep .f files)
+#
+# First the defaults
+#
+               FC := sxf90
+           FFLAGS :=
+              CPP := /usr/bin/cpp
+         CPPFLAGS :=
+               LD := $(FC)
+          LDFLAGS :=
+               AR := sxar
+          ARFLAGS := rv
+            MKDIR := mkdir -p
+               RM := rm -f
+           RANLIB := sxar -tv
+             PERL := perl
+             TEST := test
+
+        MDEPFLAGS := --cpp --fext=f90 --file=- --objdir=$(SCRATCH_DIR)
+
+#
+# Library locations, can be overridden by environment variables.
+#
+
+ifdef USE_NETCDF4
+    NETCDF_INCDIR ?= /usr/local/netcdf4/include
+    NETCDF_LIBDIR ?= /usr/local/netcdf4/lib
+      HDF5_LIBDIR ?= /usr/local/hdf5/lib
+else
+    NETCDF_INCDIR ?= /usr/local/netcdf/include
+    NETCDF_LIBDIR ?= /usr/local/netcdf/lib
+endif
+             LIBS := -L$(NETCDF_LIBDIR) -lnetcdf
+ifdef USE_NETCDF4
+             LIBS += -L$(HDF5_LIBDIR) -lhdf5_hl -lhdf5 -lz
+endif
+
+ifdef USE_ARPACK
+ ifdef USE_MPI
+   PARPACK_LIBDIR ?= /usr/local/PARPACK
+             LIBS += -L$(PARPACK_LIBDIR) -lparpack
+ endif
+    ARPACK_LIBDIR ?= /usr/local/PARPACK
+             LIBS += -L$(ARPACK_LIBDIR) -larpack
+endif
+
+ifdef USE_MPI
+         CPPFLAGS += -DMPI
+ ifdef USE_MPIF90
+               FC := sxmpif90
+ else
+             LIBS += -lfmpi -lmpi
+ endif
+endif
+
+ifdef USE_OpenMP
+         CPPFLAGS += -D_OPENMP
+           FFLAGS += -openmp -fpp
+endif
+
+ifdef USE_DEBUG
+           FFLAGS += -g -Cdebug -C e
+else
+#          FFLAGS += -Cvopt  -Csopt -sx6
+#          FFLAGS += -Cvopt -Cssafe -sx6
+           FFLAGS += -Chopt -sx6
+endif
+
+ifdef USE_MCT
+       MCT_INCDIR ?= /usr/local/mct/include
+       MCT_LIBDIR ?= /usr/local/mct/lib
+           FFLAGS += -I$(MCT_INCDIR)
+             LIBS += -L$(MCT_LIBDIR) -lmct -lmpeu
+endif
+
+ifdef USE_ESMF
+      ESMF_SUBDIR := $(ESMF_OS).$(ESMF_COMPILER).$(ESMF_ABI).$(ESMF_COMM).$(ESMF_SITE)
+      ESMF_MK_DIR ?= $(ESMF_DIR)/lib/lib$(ESMF_BOPT)/$(ESMF_SUBDIR)
+                     include $(ESMF_MK_DIR)/esmf.mk
+           FFLAGS += $(ESMF_F90COMPILEPATHS)
+             LIBS += $(ESMF_F90LINKPATHS) -lesmf -lC
+endif
+
+ifdef USE_WRF
+           FFLAGS += -I$(MCT_INCDIR)
+             LIBS += -L$(MCT_LIBDIR) -lmct -lmpeu
+             LIBS += WRF/main/module_wrf_top.o
+             LIBS += WRF/main/libwrflib.a
+             LIBS += WRF/external/fftpack/fftpack5/libfftpack.a
+             LIBS += WRF/external/io_grib1/libio_grib1.a
+             LIBS += WRF/external/io_grib_share/libio_grib_share.a
+             LIBS += WRF/external/io_int/libwrfio_int.a
+             LIBS += WRF/external/esmf_time_f90/libesmf_time.a
+             LIBS += WRF/external/RSL_LITE/librsl_lite.a
+             LIBS += WRF/frame/module_internal_header_util.o
+             LIBS += WRF/frame/pack_utils.o
+             LIBS += WRF/external/io_netcdf/libwrfio_nf.a
+#            LIBS += WRF/external/io_netcdf/wrf_io.o
+endif
+
+#
+# Use full path of compiler.
+#
+               FC := $(shell which ${FC})
+               LD := $(FC)
+
+#
+# Set free form format in source files to allow long string for
+# local directory and compilation flags inside the code.
+#
+
+$(SCRATCH_DIR)/mod_ncparam.o: FFLAGS += -f4
+$(SCRATCH_DIR)/mod_strings.o: FFLAGS += -f4
+
+#
+# Supress free format in SWAN source files since there are comments
+# beyond column 72.
+#
+
+ifdef SWAN_COUPLE
+
+$(SCRATCH_DIR)/ocpcre.o: FFLAGS += -nofree
+$(SCRATCH_DIR)/ocpids.o: FFLAGS += -nofree
+$(SCRATCH_DIR)/ocpmix.o: FFLAGS += -nofree
+$(SCRATCH_DIR)/swancom1.o: FFLAGS += -nofree
+$(SCRATCH_DIR)/swancom2.o: FFLAGS += -nofree
+$(SCRATCH_DIR)/swancom3.o: FFLAGS += -nofree
+$(SCRATCH_DIR)/swancom4.o: FFLAGS += -nofree
+$(SCRATCH_DIR)/swancom5.o: FFLAGS += -nofree
+$(SCRATCH_DIR)/swanmain.o: FFLAGS += -nofree
+$(SCRATCH_DIR)/swanout1.o: FFLAGS += -nofree
+$(SCRATCH_DIR)/swanout2.o: FFLAGS += -nofree
+$(SCRATCH_DIR)/swanparll.o: FFLAGS += -nofree
+$(SCRATCH_DIR)/swanpre1.o: FFLAGS += -nofree
+$(SCRATCH_DIR)/swanpre2.o: FFLAGS += -nofree
+$(SCRATCH_DIR)/swanser.o: FFLAGS += -nofree
+$(SCRATCH_DIR)/swmod1.o: FFLAGS += -nofree
+$(SCRATCH_DIR)/swmod2.o: FFLAGS += -nofree
+$(SCRATCH_DIR)/swmod3.o: FFLAGS += -nofree
+
+endif
+
