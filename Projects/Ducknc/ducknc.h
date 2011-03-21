1defd7812dec12e884289e00651c3664c154ffcb
john warner <jcwarner@usgs.gov>
2011-03-21 18:52:47 +0000

    updating Duck test case

---
+++ Projects/Ducknc/ducknc.h
@@ -0,0 +1,96 @@
+/*
+** svn $Id: ducknc.h 429 2009-12-20 17:30:26Z arango $
+*******************************************************************************
+** Copyright (c) 2002-2010 The ROMS/TOMS Group                               **
+**   Licensed under a MIT/X style license                                    **
+**   See License_ROMS.txt                                                    **
+*******************************************************************************
+**
+** Options for DUCK, NC Beach Test Case.
+**
+** Application flag:   DUCKNC
+** Input scripts:      ocean_ducknc.h
+**                     sediment_shoreface.h
+*/
+
+#define ROMS_MODEL
+#define WEC_VF
+#define ROLLER_RENIERS
+#undef  ROLLER_SVENDSEN
+#undef  WEC_MELLOR
+
+#define UV_VIS2
+#define MIX_S_UV
+#define DIAGNOSTICS_UV
+#define AVERAGES
+#define AVERAGES_WEC
+#define WET_DRY
+#define UV_ADV
+#define UV_C2ADVECTION
+#undef  TS_MPDATA
+#define TS_U3HADVECTION
+#define DJ_GRADPS
+#undef  SALINITY
+#define SOLVE3D
+#define SPLINES
+#define NS_PERIODIC
+
+#define EAST_M2GRADIENT
+/*
+#define EAST_M2REDUCED*/
+#define FSOBC_REDUCED
+#define EAST_M3GRADIENT
+#define EAST_TGRADIENT
+#define EAST_M2SGRADIENT
+#define EAST_M3SGRADIENT
+#define WESTERN_WALL
+
+
+#define MASKING
+
+#define ANA_FSOBC
+#define ANA_M2OBC
+#define ANA_SMFLUX
+#undef  UV_LOGDRAG
+#ifdef SOLVE3D
+# define SSW_BBL
+# ifdef SSW_BBL
+#  define SSW_CALC_ZNOT
+#  undef  SSW_LOGINT
+#  undef  SSW_ZORIP
+# endif
+
+# undef SEDIMENT
+# ifdef SEDIMENT
+#  undef  SED_MORPH
+#  define SUSPLOAD
+#  define BEDLOAD_MPM
+#  undef  BEDLOAD_SOULSBY
+#  define AVERAGES_BEDLOAD
+# endif
+
+/*# if defined SEDIMENT || defined SG_BBL || defined MB_BBL || defined SSW_BBL
+#  define ANA_SEDIMENT
+# endif
+*/
+
+# define ANA_STFLUX
+# define ANA_SSFLUX
+# define ANA_BPFLUX
+# define ANA_BTFLUX
+# define ANA_BSFLUX
+# define ANA_SPFLUX
+# define ANA_SRFLUX
+# undef  ANA_VMIX
+
+# define GLS_MIXING
+# if defined GLS_MIXING
+#  define KANTHA_CLAYSON
+#  define N2S2_HORAVG
+#  undef CRAIG_BANNER
+#  undef CHARNOK
+#  undef ZOS_HSIG
+#  undef TKE_WAVEDISS
+# endif
+
+#endif
