e4112f71fb1f69e8a42775612ee96caf4f01b32b
john warner <jcwarner@usgs.gov>
2011-03-31 13:34:29 +0000

    add rotate test

---
+++ Projects/Rotate_test/ana_m2obc.h
@@ -0,0 +1,142 @@
+      SUBROUTINE ana_m2obc (ng, tile, model)
+!
+!! svn $Id: ana_m2obc.h 429 2009-12-20 17:30:26Z arango $
+!!======================================================================
+!! Copyright (c) 2002-2010 The ROMS/TOMS Group                         !
+!!   Licensed under a MIT/X style license                              !
+!!   See License_ROMS.txt                                              !
+!=======================================================================
+!                                                                      !
+!  This routine sets 2D momentum open boundary conditions using        !
+!  analytical expressions.                                             !
+!                                                                      !
+!=======================================================================
+!
+      USE mod_param
+      USE mod_grid
+      USE mod_ncparam
+      USE mod_ocean
+      USE mod_stepping
+!
+! Imported variable declarations.
+!
+      integer, intent(in) :: ng, tile, model
+
+#include "tile.h"
+!
+      CALL ana_m2obc_tile (ng, tile, model,                             &
+     &                     LBi, UBi, LBj, UBj,                          &
+     &                     IminS, ImaxS, JminS, JmaxS,                  &
+     &                     knew(ng),                                    &
+     &                     GRID(ng) % angler,                           &
+     &                     GRID(ng) % h,                                &
+     &                     GRID(ng) % pm,                               &
+     &                     GRID(ng) % pn,                               &
+     &                     GRID(ng) % on_u,                             &
+#ifdef MASKING
+     &                     GRID(ng) % umask,                            &
+#endif
+     &                     OCEAN(ng) % zeta)
+!
+! Set analytical header file name used.
+!
+#ifdef DISTRIBUTE
+      IF (Lanafile) THEN
+#else
+      IF (Lanafile.and.(tile.eq.0)) THEN
+#endif
+        ANANAME(12)=__FILE__
+      END IF
+
+      RETURN
+      END SUBROUTINE ana_m2obc
+!
+!***********************************************************************
+      SUBROUTINE ana_m2obc_tile (ng, tile, model,                       &
+     &                           LBi, UBi, LBj, UBj,                    &
+     &                           IminS, ImaxS, JminS, JmaxS,            &
+     &                           knew,                                  &
+     &                           angler, h, pm, pn, on_u,               &
+#ifdef MASKING
+     &                           umask,                                 &
+#endif
+     &                           zeta)
+!***********************************************************************
+!
+      USE mod_param
+      USE mod_boundary
+      USE mod_grid
+      USE mod_scalars
+!
+!  Imported variable declarations.
+!
+      integer, intent(in) :: ng, tile, model
+      integer, intent(in) :: LBi, UBi, LBj, UBj
+      integer, intent(in) :: IminS, ImaxS, JminS, JmaxS
+      integer, intent(in) :: knew
+!
+#ifdef ASSUMED_SHAPE
+      real(r8), intent(in) :: angler(LBi:,LBj:)
+      real(r8), intent(in) :: h(LBi:,LBj:)
+      real(r8), intent(in) :: pm(LBi:,LBj:)
+      real(r8), intent(in) :: pn(LBi:,LBj:)
+      real(r8), intent(in) :: on_u(LBi:,LBj:)
+# ifdef MASKING
+      real(r8), intent(in) :: umask(LBi:,LBj:)
+# endif
+      real(r8), intent(in) :: zeta(LBi:,LBj:,:)
+#else
+      real(r8), intent(in) :: angler(LBi:UBi,LBj:UBj)
+      real(r8), intent(in) :: h(LBi:UBi,LBj:UBj)
+      real(r8), intent(in) :: pm(LBi:UBi,LBj:UBj)
+      real(r8), intent(in) :: pn(LBi:UBi,LBj:UBj)
+      real(r8), intent(in) :: on_u(LBi:UBi,LBj:UBj)
+# ifdef MASKING
+      real(r8), intent(in) :: umask(LBi:UBi,LBj:UBj)
+# endif
+      real(r8), intent(in) :: zeta(LBi:UBi,LBj:UBj,3)
+#endif
+!
+!  Local variable declarations.
+!
+      integer :: i, j
+      real(r8) :: angle, cff, fac, major, minor, omega, phase, val
+      real(r8) :: ramp
+#if defined ESTUARY_TEST || defined INLET_TEST
+      real(r8) :: my_area, my_flux, tid_flow, riv_flow, cff1, cff2,     &
+     &            model_flux
+#endif
+#if defined TEST_CHAN
+      real(r8) :: my_area, my_width
+#endif
+
+#include "set_bounds.h"
+!
+!-----------------------------------------------------------------------
+!  2D momentum open boundary conditions.
+!-----------------------------------------------------------------------
+!
+#if defined ROT_TEST
+      IF (EASTERN_EDGE) THEN
+        ramp=MIN(time(ng)/900.0_r8,1.0_r8)
+        DO j=JstrR,JendR
+          BOUNDARY(ng)%ubar_east(j)=0.0_r8
+          IF ((j.gt.41).and.(j.lt.54))then
+            val=0.5_r8*(zeta(Iend  ,j,knew)+h(Iend  ,j)+                &
+     &                  zeta(Iend+1,j,knew)+h(Iend+1,j))
+            BOUNDARY(ng)%ubar_east(j)=-30.0_r8/val*ramp
+          ENDIF
+# ifdef MASKING
+	  BOUNDARY(ng)%ubar_east(j)=BOUNDARY(ng)%ubar_east(j)*          &
+     &                              umask(Iend,j)
+# endif
+        END DO
+        DO j=Jstr,JendR
+          BOUNDARY(ng)%vbar_east(j)=0.0_r8
+        END DO
+      END IF
+#else
+      write(*,*) 'need to define ubar_est'
+#endif
+      RETURN
+      END SUBROUTINE ana_m2obc_tile
