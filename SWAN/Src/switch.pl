21488d3b39781be40798f25a173d8825b7d0e6ef
john warner <jcwarner@usgs.gov>
2016-05-20 18:11:39 +0000

    update to COAWST_v3.2

--- SWAN/Src/switch.pl
+++ SWAN/Src/switch.pl
@@ -11,6 +11,7 @@ $sgi = "FALSE";
 $imp = "FALSE";
 $cvi = "FALSE";
 $adc = "FALSE";
+$coh = "FALSE";
 $ncf = "FALSE";
 $mv4 = "FALSE";
 while ( $ARGV[0]=~/-.*/ )
@@ -27,6 +28,7 @@ while ( $ARGV[0]=~/-.*/ )
    if ($ARGV[0]=~/-impi/) {$imp="TRUE";shift;}
    if ($ARGV[0]=~/-cvis/) {$cvi="TRUE";shift;}
    if ($ARGV[0]=~/-adcirc/) {$adc="TRUE";shift;}
+   if ($ARGV[0]=~/-coh/) {$coh="TRUE";shift;}
    if ($ARGV[0]=~/-netcdf/) {$ncf="TRUE";shift;}
    if ($ARGV[0]=~/-matl4/) {$mv4="TRUE";shift;}
    }
@@ -67,6 +69,7 @@ foreach $file (@files)
       else               {$newline=~s/^!!ESMF//;} #second "!" is negation
       if ($tim=~/TRUE/) {$newline=~s/^!TIMG//;}
       if ($mpi=~/TRUE/) {$newline=~s/^!MPI//;}
+      if ($coh=~/TRUE/) {$newline=~s/^!MPI//;}
       if ($pun=~/TRUE/) {$newline=~s/^!PUN//;}
       if ($pun=~/FALSE/) {$newline=~s/^!NPUN//;}
       if ($f95=~/TRUE/) {$newline=~s/^!F95//;}
@@ -78,6 +81,8 @@ foreach $file (@files)
       if ($cvi=~/TRUE/) {$newline=~s/^!CVIS//;}
       if ($adc=~/TRUE/) {$newline=~s/^!ADC//;}
       if ($adc=~/FALSE/) {$newline=~s/^!NADC//;}
+      if ($coh=~/TRUE/) {$newline=~s/^!COH//;}
+      if ($coh=~/FALSE/){$newline=~s/^!NCOH//;}
       if ($ncf=~/TRUE/) {$newline=~s/^!NCF//;}
       if ($ncf=~/FALSE/){$newline=~s/^!NNCF//;}
       if ($mv4=~/TRUE/) {$newline=~s/^!MatL4//;}
