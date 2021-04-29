ec6368a0e77cb1aa28f9c75235df693dcaceb146
jcwarner <jcwarner@48cfb302-6b48-4dc2-989a-fd0c5d807893>
2021-04-29 13:37:15 +0000

    update ROMS into COAWST v3.7
    git-svn-id: https://coawstmodel.sourcerepo.com/coawstmodel/COAWST@1545 48cfb302-6b48-4dc2-989a-fd0c5d807893

---
+++ ROMS/Nonlinear/SeaIce/Module.mk
@@ -0,0 +1,15 @@
+# svn $Id: Module.mk 1054 2021-03-06 19:47:12Z arango $
+#::::::::::::::::::::::::::::::::::::::::::::::::::::: Hernan G. Arango :::
+# Copyright (c) 2002-2021 The ROMS/TOMS Group             Kate Hedstrom :::
+#   Licensed under a MIT/X style license                                :::
+#   See License_ROMS.txt                                                :::
+#::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+
+local_sub  := ROMS/Nonlinear/SeaIce
+
+local_lib  := libICE.a
+local_src  := $(wildcard $(local_sub)/*.F)
+
+$(eval $(call make-library,$(local_lib),$(local_src)))
+
+$(eval $(compile-rules))
