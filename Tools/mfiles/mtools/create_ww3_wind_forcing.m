d41bc8ed5b930c7d71df6657c56e54c0d109615f
jcwarner-USGS <jcwarner@usgs.gov>
2024-06-10 14:58:23 -0400

    add Cd to output

--- Tools/mfiles/mtools/create_ww3_wind_forcing.m
+++ Tools/mfiles/mtools/create_ww3_wind_forcing.m
@@ -9,13 +9,17 @@ netcdf_load('../romsforc_NARR_Sandy2012.nc')
 fid = fopen('ww3_sandy_wind_forc.dat','w');
 for mm=1:ntimes
 % write time stamp
-  zz=datevec(wind_time(mm)+datenum(1858,11,17,0,0,0));
+  zz=datevec(double(wind_time(mm))+datenum(1858,11,17,0,0,0));
   zz1=num2str(zz(1));
   zz2=['00',num2str(zz(2))];zz2=zz2(end-1:end);
   zz3=['00',num2str(zz(3))];zz3=zz3(end-1:end);
-  zz4=['00',num2str(zz(4))];zz4=zz4(end-1:end);
-  zz5=['00',num2str(zz(5))];zz5=zz5(end-1:end);
-  zz6=['00',num2str(zz(6))];zz6=zz6(end-1:end);
+% zz4=['00',num2str(zz(4))];zz4=zz4(end-1:end);
+% zz5=['00',num2str(zz(5))];zz5=zz5(end-1:end);
+% zz6=['00',num2str(zz(6))];zz6=zz6(end-1:end);
+  aa=round(zz(4)+zz(5)/100);
+  zz4=['00',num2str(aa)];zz4=zz4(end-1:end);
+  zz5=['00'];
+  zz6=['00'];
   dtstr=[zz1,zz2,zz3,' ',zz4,zz5,zz6]
   fprintf(fid,'%15s',dtstr);
   fprintf(fid,'\n');
