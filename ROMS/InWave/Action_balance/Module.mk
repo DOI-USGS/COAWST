2b2e74eccd1ee54a8a909bd97932786ee3ecdfd5
U-GS\jcwarner <jcwarner@usgs.gov>
2026-07-17 09:50:50 -0400

    updating to COAWST v3.9

--- ROMS/InWave/Action_balance/Module.mk
+++
@@ -1,9 +0,0 @@
-# svn $Id: Module.mk 1311 2008-01-10 04:13:52Z jcwarner $
-
-local_sub  := InWave/Action_balance
-
-local_src  := $(wildcard $(local_sub)/*.F)
-
-sources    += $(local_src)
-
-$(eval $(compile-rules))
--- ROMS/InWave/Utility/Module.mk
+++
@@ -1,9 +0,0 @@
-# svn $Id: Module.mk 1311 2008-01-10 04:13:52Z jcwarner $
-
-local_sub  := InWave/Utility
-
-local_src  := $(wildcard $(local_sub)/*.F)
-
-sources    += $(local_src)
-
-$(eval $(compile-rules))
